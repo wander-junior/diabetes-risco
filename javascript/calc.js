c58b40cc81d6cca53023e26b6d433434d73c2021
Wander <wanderjrcruz96@gmail.com>
2020-04-03 14:49:11 -0300

    Cria pergunta sobre medicamentos

--- javascript/calc.js
+++ javascript/calc.js
@@ -4,16 +4,7 @@ let result = 0
 let weight = 0
 let height = 0
 
-// Começa o teste pela pergunta da idade
-beggin.onclick = function() {
-    criaTitulo("Idade")
-    let ageField = document.createElement("input")
-    ageField.type = "number"
-    testField.appendChild(ageField)
-    ageField.id = "age"
-    age = document.querySelector("#age")
-    criaBtn("calcIdade()")
-}
+// Funções para facilitar a criação das perguntas (criaTitulo, criaBtn, criaSelect)
 
 function criaTitulo(titulo){
     testField.innerHTML = ""
@@ -27,6 +18,34 @@ function criaBtn(funcao) {
     nextBtn.innerHTML = "Próximo"
     nextBtn.setAttribute('onclick', funcao)
     testField.appendChild(nextBtn)
+}
+
+function criaSelect(option1, option2, value1=option1, value2=option2) {
+    let selectField = document.createElement("select")
+    testField.appendChild(selectField)
+
+    let select1 = document.createElement("option")
+    select1.setAttribute("value", value1)
+    let select1Value = document.createTextNode(option1)
+    select1.appendChild(select1Value)
+    document.querySelector("select").appendChild(select1)
+
+    let select2 = document.createElement("option")
+    select2.setAttribute("value", value2)
+    let select2Value = document.createTextNode(option2)
+    select2.appendChild(select2Value)
+    document.querySelector("select").appendChild(select2)
+}
+
+// Começa o teste pela pergunta da idade
+beggin.onclick = function() {
+    criaTitulo("Idade")
+    let ageField = document.createElement("input")
+    ageField.type = "number"
+    testField.appendChild(ageField)
+    ageField.id = "age"
+    age = document.querySelector("#age")
+    criaBtn("calcIdade()")
 }
 
 function calcIdade() {
@@ -55,23 +74,6 @@ function perguntaSexo() {
 
     criaBtn("perguntaCintura()")
     sexo = document.querySelector('select')
-}
-
-function criaSelect(option1, option2, value1=option1, value2=option2) {
-    let selectField = document.createElement("select")
-    testField.appendChild(selectField)
-
-    let select1 = document.createElement("option")
-    select1.setAttribute("value", value1)
-    let select1Value = document.createTextNode(option1)
-    select1.appendChild(select1Value)
-    document.querySelector("select").appendChild(select1)
-
-    let select2 = document.createElement("option")
-    select2.setAttribute("value", value2)
-    let select2Value = document.createTextNode(option2)
-    select2.appendChild(select2Value)
-    document.querySelector("select").appendChild(select2)
 }
 
 function perguntaCintura() {
@@ -167,11 +169,22 @@ function perguntaAtvFisica() {
 
 function perguntaDieta() {
     //Faz pontuação da pergunta anterior
-    if(atvFisica.value = 'nao') {
+    if(atvFisica.value == 'nao') {
         result += 2
     }
     criaTitulo("Com que regularidade come vegetais e/ou fruta (sopa, salada, legumes cozidos, entre outros)?")
     criaSelect("Sim", "Não", "sim", "nao")
-    criaBtn()
+    criaBtn("perguntaMedicamentos()")
     dieta = document.querySelector('select')
+}
+
+function perguntaMedicamentos() {
+    //Faz pontuação da pergunta anterior
+    if(dieta.value == 'nao') {
+        result += 1
+    }
+    criaTitulo("Toma regularmente ou já tomou alguns medicamentos para a Hipertensão Arterial?")
+    criaSelect("Sim", "Não", "sim", "nao")
+    criaBtn()
+    medicacao = document.querySelector('select')
 }
